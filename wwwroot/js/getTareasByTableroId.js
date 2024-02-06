931e7e019ab0f5d4bac1c35381a4e947ad6f9429
Tomás Cassas <tcassas@archt.com.ar>
2024-02-06 16:26:22 -0300

    Modal para cambiar el estado de una tarea

--- wwwroot/js/getTareasByTableroId.js
+++ wwwroot/js/getTareasByTableroId.js
@@ -3,6 +3,13 @@ $("#modal-detalle-tarea").on("show.bs.modal", (event) => {
   const tareaNombre = button.getAttribute("data-bs-tareaNombre");
   const tareaDescripcion = button.getAttribute("data-bs-tareaDescripcion")
 
-  $(".modal-title").text(tareaNombre);
-  $(".modal-body").text(tareaDescripcion);
+  $("#detalle-tarea-nombre").text(tareaNombre);
+  $("#detalle-tarea-descripcion").text(tareaDescripcion);
+})
+
+$("#modal-cambiar-estado-tarea").on("show.bs.modal", (event) => {
+  const button = event.relatedTarget;
+  const tareaNombre = button.getAttribute("data-bs-tareaNombre");
+
+  $("#cambiar-estado-nombre").text(tareaNombre);
 })
