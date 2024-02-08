3241d9b352a70ac7d3d7ed1a171da917adb8e0f0
Tomás Cassas <tcassas@archt.com.ar>
2024-02-08 16:06:32 -0300

    Estado inicial refactor ui pagina home con datos del usuario, actividad y tableros

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using tl2_tp10_2023_TcassasT.Interfaces;
+using tl2_tp10_2023_TcassasT.Models;
 using tl2_tp10_2023_TcassasT.tl2_tp10_2023_TcassasT.Models;
 
 namespace tl2_tp10_2023_TcassasT.Controllers;
@@ -7,25 +9,40 @@ namespace tl2_tp10_2023_TcassasT.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IUsuarioTableroRepository _usuarioTableroRepository;
+    private readonly ITableroReposiroty _tableroReposiroty;
 
-    public HomeController(ILogger<HomeController> logger)
-    {
+    public HomeController(ILogger<HomeController> logger, IUsuarioTableroRepository usuarioTableroRepository, ITableroReposiroty tableroReposiroty) {
         _logger = logger;
+        _usuarioTableroRepository = usuarioTableroRepository;
+        _tableroReposiroty = tableroReposiroty;
     }
 
-    public IActionResult Index()
-    {
-        return View();
+    public IActionResult Index() {
+        int? usuarioId = HttpContext.Session.GetInt32("UsuarioId");
+
+        if (usuarioId == null) {
+            throw new Exception("Usuario no está logueado");
+        }
+
+        List<int> tablerosPertenecientes = new List<int>();
+        List<UsuarioTablero> membresias = _usuarioTableroRepository.GetMembresias((int) usuarioId);
+        membresias.ForEach((UsuarioTablero membresia) => {
+            tablerosPertenecientes.Add(membresia.TableroId);
+        });
+        List<Tablero> tableros = _tableroReposiroty.GetTablerosByTableroId(tablerosPertenecientes);
+
+        HomeIndexViewModel homeIndexViewModel = new HomeIndexViewModel() { tableros = tableros };
+
+        return View(homeIndexViewModel);
     }
 
-    public IActionResult Privacy()
-    {
+    public IActionResult Privacy() {
         return View();
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
+    public IActionResult Error() {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
